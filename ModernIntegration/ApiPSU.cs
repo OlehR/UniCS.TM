1d267802359d39f4e65e683b40d1410117e12560
OlehR <OlehRut@gmail.com>
2019-11-04 16:03:56 +0200

    +await

--- ModernIntegration/ApiPSU.cs
+++ ModernIntegration/ApiPSU.cs
@@ -423,9 +423,8 @@ namespace ModernIntegration
                 info.StatusDescription = ex.Message;
             }
             OnSyncInfoCollected?.Invoke(info);
-#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-            Bl.SendAllReceipt();
-#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+
+            await Bl.SendAllReceipt().ConfigureAwait(false);
         }
 
         private static Api _instance;
