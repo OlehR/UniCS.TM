008c311d7bc75a179b764e2240649aea18ba6f4a
OlehR <OlehRut@gmail.com>
2020-02-03 11:46:29 +0200

    AddWeight

---
+++ ModelMID/DB/AddWeight.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelMID.DB
+{
+    public class AddWeight
+    {
+        public int CodeWares { get; set; }
+        public int CodeUnit { get; set; }
+        public decimal Weight { get; set; }
+
+    }
+}
