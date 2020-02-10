6af11e0f83b7c4189daf0dc4da165ffdd6d39ef9
OlehR <OlehRut@gmail.com>
2020-02-10 13:09:08 +0200

    add BarCodeOut

---
+++ ModelMID/DB/BarCodeOut.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelMID.DB
+{
+    public class BarCodeOut
+    {
+        public string BarCode { get; set; }
+        public decimal Weight { get; set; }
+        public DateTime Date { get; set; }
+        public string Url { get; set; }
+        public string Data { get; set; }
+        public decimal WeightUrl { get; set; }
+        public DateTime DateUrl { get; set; }
+        public string Error { get; set; }
+    }
+
+}
