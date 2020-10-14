ab93c16d10accf89d2cf2c5c772db0ced4eef578
OlehR <OlehRut@gmail.com>
2020-10-14 11:18:23 +0300

    front

--- Front/Equipments/BankTerminal.cs
+++ Front/Equipments/BankTerminal.cs
@@ -7,6 +7,9 @@ namespace Front.Equipments
     public class BankTerminal:Equipment
     {
         public BankTerminal(string pSerialPortName, int pBaudRate = 9600, Action<string, string> pLogger = null) : base(pSerialPortName, pBaudRate) { }
-        public void SendMoney(decimal pSum) { throw new NotImplementedException(); }
+        public void Purchase(decimal pAmount) { throw new NotImplementedException(); }
+        public void Refund(decimal pAmount, string pRRN) { throw new NotImplementedException(); }
+
+
     }
 }
