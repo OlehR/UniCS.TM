9fe841e402523bad558d279fe5590aa7e199f8aa
OlehR <OlehRut@gmail.com>
2021-11-10 16:51:15 +0200

    Парсінг.

--- ModelMID/DB/BarCodeOut.cs
+++ ModelMID/DB/BarCodeOut.cs
@@ -7,6 +7,7 @@ namespace ModelMID.DB
     public class BarCodeOut
     {
         public int CodeWares { get; set; }
+        public string NameWares { get; set; }
         public string BarCode { get; set; }
         public decimal Weight { get; set; }
         public DateTime Date { get; set; }
@@ -16,7 +17,35 @@ namespace ModelMID.DB
         public DateTime DateUrl { get; set; }
         public string Error { get; set; }        
         public string UrlPicture { get; set; }
+        /// <summary>
+        /// Чи актуальна інформація
+        /// </summary>
+        public bool IsActual { get; set; }
+        /// <summary>
+        /// Чи верифікований товар.
+        /// </summary>
+        public bool IsVerification { get; set; }
+        public string Site { get; set; }
+        public string Unit1 { get; set; }
+        public string Unit2 { get; set; }
+        public string Unit3 { get; set; }
+        public string Unit4 { get; set; }
+        public string Unit5 { get; set; }
+        public string Name { get; set; }
+        public string NameShort { get; set; }
+        public string Other { get; set; }
+        public string UKTZED { get; set; }
+        public int VAT { get; set; }
+        /// <summary>
+        /// термін придатності в днях
+        /// </summary>
+        public decimal ExpirationDay { get; set; }
 
+        public decimal UnitSale { get; set; }
+        public string  UnitSaleName { get; set; }
+        public string PaletteLayer { get; set; }
+        public string Palette { get; set; }
+ 
     }
 
 }
