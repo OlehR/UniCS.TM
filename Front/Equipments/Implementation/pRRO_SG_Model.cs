fa9c9c0c1ed24b9358273c943813bdee3969a78b
sevcukd <sevcukd9@gmail.com>
2021-12-13 11:49:19 +0200

    checkbox added classes

--- Front/Equipments/Implementation/pRRO_SG_Model.cs
+++ Front/Equipments/Implementation/pRRO_SG_Model.cs
@@ -51,7 +51,7 @@ namespace Front.Equipments.pRRO_SG
 
     public class Record
     {
-       
+
         public eTypeRecord type { get; set; }
         public int number { get; set; }
         public Record(eTypeRecord pType = eTypeRecord.SKU) { type = pType; }
@@ -65,7 +65,7 @@ namespace Front.Equipments.pRRO_SG
             SKU = pRW.CodeWares;
             UKTZED = pRW.CodeUKTZED;
             barcode = pRW.BarCode;
-            if(!string.IsNullOrEmpty( pRW.ExciseStamp))
+            if (!string.IsNullOrEmpty(pRW.ExciseStamp))
                 excises = pRW.ExciseStamp.Split(',');
             name = pRW.NameWares;
             price = Convert.ToInt32(pRW.PriceEKKA * 100);
@@ -171,14 +171,14 @@ namespace Front.Equipments.pRRO_SG
     public class RecordPay : Record
     {
         public RecordPay() : base(eTypeRecord.PAY) { }
-        public RecordPay(Payment pP) : base(eTypeRecord.PAY) 
+        public RecordPay(Payment pP) : base(eTypeRecord.PAY)
         {
-            sum = Convert.ToInt32( pP.SumPay*100m);
+            sum = Convert.ToInt32(pP.SumPay * 100m);
             returnSum = 0;
             typePay = (pP.TypePay == ModelMID.eTypePay.Card ? eTypePay.Card : eTypePay.Cash);
-            if(pP.TypePay == ModelMID.eTypePay.Card)
+            if (pP.TypePay == ModelMID.eTypePay.Card)
             {
-                cards = new List<Card>() { new Card(pP, sum>0) };
+                cards = new List<Card>() { new Card(pP, sum > 0) };
             }
         }
         public int sum { get; set; }
@@ -206,7 +206,7 @@ namespace Front.Equipments.pRRO_SG
             Sum = pR.SumReceipt;
             docSubType = pR.TypeReceipt == eTypeReceipt.Refund ? eTypeDoc.Refund : eTypeDoc.Sale;
             id = pR.ReceiptId;
-            var b= new List<Record>();
+            var b = new List<Record>();
             foreach (var el in pR.Wares)
                 b.Add(new RecordSKU(el));
 
@@ -284,5 +284,178 @@ namespace Front.Equipments.pRRO_SG
         /// </summary>
         public int receiptNumber { get; set; }
     }
+
+    // створення чеку
+    public class CreateReceipt
+    {
+        public string departament { get; set; }
+        public string cashier_name { get; set; }
+        public string header { get; set; }
+        public string footer { get; set; }
+        public string barcode { get; set; }
+        public IEnumerable<Discount> discounts { get; set; }
+    }
+    // кінець створення чека
+
+    // додавання товару
+    public class AddWares
+    {
+        public string code { get; set; }
+        public string name { get; set; }
+        public string price { get; set; }
+        public string quantity { get; set; }
+        public IEnumerable<TaxWares> taxes { get; set; }
+        public IEnumerable<Discount> discounts { get; set; }
+        public string uktzed { get; set; }
+        public IEnumerable<Excise_barcod> excise_barcodes { get; set; }
+        public string barcode { get; set; }
+        public bool is_return { get; set; }
+    }
+
+    public class TaxWares
+    {
+        public int tax { get; set; }
+    }
+    public class Excise_barcod
+    {
+        public string excise_barcod { get; set; }
+    }
+    // кінець додавання товару
+
+    public class Discount
+    {
+        public string type { get; set; } // "DISCOUNT", string??
+        public string mode { get; set; } // "PERCENT", string??
+        public int value { get; set; }
+        public string name { get; set; }
+        public IEnumerable<Tax_code> tax_codes { get; set; }
+    }
+    public class Tax_code
+    {
+        public int tax_code { get; set; }
+    }
+
+    // Get Shift
+    public class GetShift
+    {
+        public int id { get; set; }
+        public string external_id { get; set; } // (3fa85f64-5717-4562-b3fc-2c963f66afa6) - string?
+        public int serial { get; set; }
+        public DateTime opened_at { get; set; }
+        public DateTime closed_at { get; set; }
+        public DateTime created_at { get; set; }
+        public DateTime updated_at { get; set; }
+        public Balance balance { get; set; }
+        public IEnumerable<TaxGetShift> taxes { get; set; }
+        public IEnumerable<PaymentGetShift> payments { get; set; }
+
+    }
+    public class Balance
+    {
+        public double initial { get; set; } // int?
+        public double balance { get; set; }
+        public double cash_sales { get; set; }
+        public double card_sales { get; set; }
+        public double cash_returns { get; set; }
+        public double card_returns { get; set; }
+        public double service_in { get; set; } // int?
+        public double service_out { get; set; } // int?
+        public DateTime updated_at { get; set; }
+    }
+    public class TaxGetShift
+    {
+        public int code { get; set; }
+        public string label { get; set; }
+        public string symbol { get; set; }
+        public int rate { get; set; }
+        public int extra_rate { get; set; }
+        public bool included { get; set; }
+        public DateTime created_at { get; set; }
+        public int sales { get; set; }
+        public int returns { get; set; }
+        public double sales_turnover { get; set; }
+        public double returns_turnover { get; set; }
+    }
+    public class PaymentGetShift
+    {
+        public string type { get; set; }
+        public string label { get; set; }
+        public int code { get; set; }
+        public int sales { get; set; }
+        public int returns { get; set; }
+        public double service_in { get; set; } // int?
+        public double service_out { get; set; } // int?
+    }
+    // Get Shift END
+
+
+
+    // Close Receipt
+    public class CloseReceipt
+    {
+        public IEnumerable<PaymentCloseReceipt> payments { get; set; }
+        public Delivery delivery { get; set; }
+        public bool print { get; set; }
+        public bool remove_rest { get; set; }
+        public bool technical_return { get; set; }
+        public Context context { get; set; }
+        public bool round { get; set; }
+    }
+    public class PaymentCloseReceipt
+    {
+        public string type;
+        public int code;
+        public double value;
+        public string label;
+        public string card_mask;
+        public string bank_name;
+        public string auth_code;
+        public string rrn;
+        public string payment_system;
+        public string owner_name;
+        public string terminal;
+        public string acquirer_and_seller;
+        public string receipt_no;
+        public bool signature_required;
+
+        public PaymentCloseReceipt() { }
+        public PaymentCloseReceipt(string type, double value, string label)
+        {
+            this.type = type;
+            this.value = value;
+            this.label = label;
+        }
+        public PaymentCloseReceipt(string type, int code, double value, string label, string card_mask, string bank_name, string auth_code, string rrn,
+            string payment_system, string owner_name, string terminal, string acquirer_and_seller, string receipt_no, bool signature_required)
+        {
+            this.type = type;
+            this.code = code;
+            this.value = value;
+            this.label = label;
+            this.card_mask = card_mask;
+            this.bank_name = bank_name;
+            this.auth_code = auth_code;
+            this.rrn = rrn;
+            this.payment_system = payment_system;
+            this.owner_name = owner_name;
+            this.terminal = terminal;
+            this.acquirer_and_seller = acquirer_and_seller;
+            this.receipt_no = receipt_no;
+            this.signature_required = signature_required;
+
+        }
+
+    }
+    public class Delivery
+    {
+        public string email { get; set; }
+    }
+    public class Context
+    {
+        public double additionalProp1 { get; set; }
+        public double additionalProp2 { get; set; }
+        public double additionalProp3 { get; set; }
+    }
+    // Close Receipt END
 }
 
