fb24764b8def1b8372d1bf448c1750d9567d1f16
sevcukd <sevcukd9@gmail.com>
2022-03-22 19:34:09 +0200

    fix bag end add log

--- ResizePhoto/Program.cs
+++ ResizePhoto/Program.cs
@@ -2,14 +2,17 @@
 using System.Linq;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
-
+using System.IO;
+using System.Threading.Tasks;
 
 namespace ResizePhoto
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            ///////////////////////////// ДЛЯ РОБОТИ ПРОГРАМИ ПОТРІБНО ДОДАТИ СИСТЕМНИЙ ШЛЯХ ВІНДОВС ДО IrfanView x64!!!!!!!!!!!!!!!!!!
+
             ObservableCollection<PhotoInfo> InformPhotoHigh = new ObservableCollection<PhotoInfo>();
             ObservableCollection<PhotoInfo> InformPhotoMedium = new ObservableCollection<PhotoInfo>();
             ObservableCollection<PhotoInfo> InformPhotoLow = new ObservableCollection<PhotoInfo>();
@@ -17,12 +20,26 @@ namespace ResizePhoto
 
             ////////////////////////  ВИНЕСТИ ЯК ЗМІННІ КУДИСЬ (файлик або конфіг файл)///////////////////////////
             string HightPhotoPath = @"d:\Pictures\Test\High\";   //шлях де брати
-            string MediumPhotoPath = @"d:\Pictures\Test\Low\";   //куди класти великої якості
-            string LowPhotoPath = @"d:\Pictures\Test\Medium\";   //куди класти низької якось
+            string MediumPhotoPath = @"d:\Pictures\Test\Medium\";//куди класти великої якості
+            string LowPhotoPath = @"d:\Pictures\Test\Low\";      //куди класти низької якось
             double mediumSize = 600;                             //Найменьша сторона до якої зводити для СЕРЕДНЬОї якость
             double lowSize = 300;                                //Найменьша сторона до якої зводити для НИЗЬКОЇ якость
+            string pathLog = @"d:\Pictures\Test\Log.txt";        //куди писати лог
+            
+            
 
-
+            // Перезапис файлу
+            //using (StreamWriter writer = new StreamWriter(pathLog, false))
+            //{
+            //    await writer.WriteLineAsync(text);
+            //}
+            // добавление в файл
+            using (StreamWriter writer = new StreamWriter(pathLog, true))
+            {
+                await writer.WriteLineAsync("//////////////////////////////////////////");
+                await writer.WriteLineAsync("Запуск програми: "+DateTime.Now.ToString());
+                await writer.WriteLineAsync("//////////////////////////////////////////");
+            }
             GetInfoFiles(HightPhotoPath, InformPhotoHigh); 
             GetInfoFiles(MediumPhotoPath, InformPhotoMedium);
             GetInfoFiles(LowPhotoPath, InformPhotoLow);
@@ -52,24 +69,39 @@ namespace ResizePhoto
             {
                 if (InformPhotoMedium.Count == 0 || InformPhotoLow.Count == 0) // Якщо папки взагалі пусті
                 {
-                    ResizePhoto(HightPhotoPath, MediumPhotoPath, highPhoto.Name, mediumSize);
-                    ResizePhoto(HightPhotoPath, LowPhotoPath, highPhoto.Name, lowSize);
+                    using (StreamWriter writer = new StreamWriter(pathLog, true))
+                    {
+                        await writer.WriteLineAsync("Знайдено відсутнє фото! Запуск обробки: " + highPhoto.Name);
+                    }
+                    _ = ResizePhotoAsync(HightPhotoPath, MediumPhotoPath, highPhoto.Name, mediumSize);
+                    _ = ResizePhotoAsync(HightPhotoPath, LowPhotoPath, highPhoto.Name, lowSize);
                 }
                 else
                 {
+                    var item = InformPhotoMedium.FirstOrDefault(i => i.Name == highPhoto.Name); // вже робоча))) фігня яка мала додати фото якщо такого ще не існує
+                    if (item == null)
+                    {
+                        using (StreamWriter writer = new StreamWriter(pathLog, true))
+                        {
+                            await writer.WriteLineAsync("Знайдено відсутнє фото! Запуск обробки: " + highPhoto.Name);
+                        }
+                        _ = ResizePhotoAsync(HightPhotoPath, MediumPhotoPath, highPhoto.Name, mediumSize);
+                        _ = ResizePhotoAsync(HightPhotoPath, LowPhotoPath, highPhoto.Name, lowSize);
+                        
+                        continue;
+                    }
                     foreach (var mediumPhoto in InformPhotoMedium)
                     {
-                        var item = InformPhotoMedium.FirstOrDefault(i => i.Name == highPhoto.Name); // неробоча фігня яка мала додати фото якщо такого ще не існує
-                        if (item == null)
-                        {
-                            ResizePhoto(HightPhotoPath, MediumPhotoPath, highPhoto.Name, mediumSize);
-                            ResizePhoto(HightPhotoPath, LowPhotoPath, highPhoto.Name, lowSize);
-                            continue;
-                        }
+
                         if (highPhoto.Name == mediumPhoto.Name && highPhoto.CreateDate > mediumPhoto.CreateDate) // Власне сама перевірка на дату та час
                         {
-                            ResizePhoto(HightPhotoPath, MediumPhotoPath, highPhoto.Name, mediumSize);
-                            ResizePhoto(HightPhotoPath, LowPhotoPath, highPhoto.Name, lowSize);
+                            
+                            using (StreamWriter writer = new StreamWriter(pathLog, true))
+                            {
+                                await writer.WriteLineAsync("Знайдено новішу картинку! Запуск оновлення картинки: " + highPhoto.Name);
+                            }
+                            _ = ResizePhotoAsync(HightPhotoPath, MediumPhotoPath, highPhoto.Name, mediumSize);
+                            _ = ResizePhotoAsync(HightPhotoPath, LowPhotoPath, highPhoto.Name, lowSize);
                             continue;
                         }
                     }
@@ -77,7 +109,11 @@ namespace ResizePhoto
                 }
 
             }
-
+            using (StreamWriter writer = new StreamWriter(pathLog, true))
+            {
+                await writer.WriteLineAsync("Програма успішно завершила роботу!");
+                await writer.WriteLineAsync("Час завершення роботи: " +DateTime.Now.ToString());
+            }
             //Інформація про файли
             void GetInfoFiles(string path, ObservableCollection<PhotoInfo> InformPhoto)
             {
@@ -95,12 +131,16 @@ namespace ResizePhoto
             //ResizePhoto(@"d:\Pictures\Categories\", @"d:\Pictures\Categories\", "test.png");
 
             //зміна якості файлу через програмку
-            void ResizePhoto(string StartPath, string EndPath, string name, double size)
+            async Task ResizePhotoAsync(string StartPath, string EndPath, string name, double size)
             {
                 ProcessStartInfo psi = new ProcessStartInfo();
                 psi.FileName = "cmd";
                 psi.Arguments = @" /c i_view64.exe " + StartPath + name + @" /resize_short=" + size + " /resample /aspectratio /convert=" + EndPath + name;
                 Process.Start(psi);
+                using (StreamWriter writer = new StreamWriter(pathLog, true))
+                {
+                    await writer.WriteLineAsync("Катринку " + name + " відредаговано та скопійовано: " + EndPath + " Розмір найкоротшої сторони: " + size);
+                }
             }
         }
 
