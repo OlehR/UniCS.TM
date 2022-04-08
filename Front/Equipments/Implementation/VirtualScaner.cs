49ae42ff2a3efbbe14f025216899238a2934b759
OlehR <OlehRut@gmail.com>
2022-04-08 14:24:48 +0300

    add Virtual scaner & Scale

---
+++ Front/Equipments/Implementation/VirtualScaner.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Front.Equipments.Implementation
+{
+    public class VirtualScaner:Scaner
+    {
+       
+        public VirtualScaner(IConfiguration pConfiguration, Action<string, string> pLogger, Action<string, string> pOnBarCode) : base(pConfiguration, pLogger, pOnBarCode)
+        {
+            if (pOnBarCode != null)
+            { }
+                
+        }
+
+    }
+}
