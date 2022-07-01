1aaf5ffb253a500fcc35d305e247413d0b75e138
sevcukd <sevcukd9@gmail.com>
2022-07-01 15:28:21 +0300

    xxx

--- Front/MainWindow.xaml.cs
+++ Front/MainWindow.xaml.cs
@@ -21,6 +21,7 @@ using SharedLib;
 using Utils;
 using Microsoft.Web.WebView2.Core;
 using Microsoft.Extensions.Configuration;
+using System.Windows.Media;
 
 namespace Front
 {
@@ -780,6 +781,12 @@ namespace Front
             _MoneySum = ListWares.Sum(r => r.SumTotal);
             MoneySum = _MoneySum.ToString();
             WaresQuantity = ListWares.Count().ToString();
+            if (VisualTreeHelper.GetChildrenCount(WaresList) > 0)
+            {
+                Border border = (Border)VisualTreeHelper.GetChild(WaresList, 0);
+                ScrollViewer scrollViewer = (ScrollViewer)VisualTreeHelper.GetChild(border, 0);
+                scrollViewer.ScrollToBottom();
+            }
             //SV_WaresList.ScrollToEnd();
         }
 
@@ -916,7 +923,7 @@ namespace Front
 
         private void _ButtonHelp(object sender, RoutedEventArgs e)
         {
-            SetStateView(eStateMainWindows.WaitAdmin);
+            SetWaitConfirm(eTypeAccess.AdminPanel);
         }
 
         private void _OwnBag(object sender, RoutedEventArgs e)
