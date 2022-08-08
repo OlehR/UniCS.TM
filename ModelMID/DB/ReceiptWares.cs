f7d0d52306f46ab0f0d5960545e4dfc0e663c222
sevcukd <sevcukd9@gmail.com>
2022-08-08 16:36:07 +0300

    Update ReceiptWares.cs

--- ModelMID/DB/ReceiptWares.cs
+++ ModelMID/DB/ReceiptWares.cs
@@ -395,7 +395,7 @@ namespace ModelMID
             }
         }
 
-        public bool IsPlus { get { return Parent?.IsLockChange != true && !IsWeight && (MaxRefundQuantity == null || Quantity < MaxRefundQuantity) && IsLast; } } // { get; set; } = false;//
+        public bool IsPlus { get { return (Parent?.IsLockChange != true && !IsWeight && (MaxRefundQuantity == null || Quantity < MaxRefundQuantity) && IsLast) && TypeWares!=eTypeWares.Tobacco; } } // { get; set; } = false;//
 
         public bool IsMinus { get { return Parent?.IsLockChange != true && !IsWeight && Quantity > 1 && IsLast; } } //{ get; set; } = false;//
 
