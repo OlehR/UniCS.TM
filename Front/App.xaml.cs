9cea925663521ea2edc78cdcb37340eeb1a609e1
Oleg Rutkovskyi <OlehRut@gmail.com>
2022-09-15 16:28:10 +0300

    Логування не перехоплених ексепшен.

--- Front/App.xaml.cs
+++ Front/App.xaml.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Globalization;
 using SharedLib;
-
+using Utils;
 namespace Front
 {
     /// <summary>
@@ -23,6 +23,12 @@ namespace Front
 		{
 			InitializeComponent();			
 		}
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            SetupExceptionHandling();
+        }
 
         private void Application_Startup(object sender, StartupEventArgs e)
         {
@@ -84,5 +90,44 @@ namespace Front
 				LanguageChanged(Application.Current, new EventArgs());
 			}
 		}
-	}
+
+        private void SetupExceptionHandling()
+        {
+            AppDomain.CurrentDomain.UnhandledException += (s, e) =>
+                LogUnhandledException((Exception)e.ExceptionObject, "AppDomain.CurrentDomain.UnhandledException");
+
+            DispatcherUnhandledException += (s, e) =>
+            {
+                LogUnhandledException(e.Exception, "Application.Current.DispatcherUnhandledException");
+                e.Handled = true;
+            };
+
+            TaskScheduler.UnobservedTaskException += (s, e) =>
+            {
+                LogUnhandledException(e.Exception, "TaskScheduler.UnobservedTaskException");
+                e.SetObserved();
+            };
+        }
+
+        private void LogUnhandledException(Exception exception, string source)
+        {
+            string message = $"Unhandled exception ({source})";
+            try
+            {
+                System.Reflection.AssemblyName assemblyName = System.Reflection.Assembly.GetExecutingAssembly().GetName();
+                message = string.Format("Unhandled exception in {0} v{1}", assemblyName.Name, assemblyName.Version);
+            }
+            catch (Exception ex)
+            {
+                FileLogger.WriteLogMessage(this, "Exception in LogUnhandledException", ex);
+               
+            }
+            finally
+            {
+                FileLogger.WriteLogMessage(this, message, exception);              
+            }
+        }
+
+
+    }
 }
