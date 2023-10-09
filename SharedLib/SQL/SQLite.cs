116e07702567902196e3a5cd5dbb221e416629a6
Oleg Rutkovskyi <OlehRut@gmail.com>
2023-10-09 10:20:41 +0300

    Підготовка до оновлення Dapper

--- SharedLib/SQL/SQLite.cs
+++ SharedLib/SQL/SQLite.cs
@@ -9,21 +9,23 @@ using System.Threading;
 using System.Threading.Tasks;
 using Utils;
 using System.Linq;
+using System.Collections;
+using System.Xml.Linq;
 
 namespace SharedLib
-{        
-    public class SQLite:SQL,IDisposable
+{
+    public class SQLite : SQL, IDisposable
     {
         public SQLiteConnection connection = null;
         SQLiteTransaction transaction = null;
         private bool disposedValue;
 
-        public SQLite(String varConectionString):base(varConectionString)
+        public SQLite(String varConectionString) : base(varConectionString)
         {
 
             var connectionString = new SQLiteConnectionStringBuilder("Data Source=" + varConectionString + ";Version=3;")
             {
-                DefaultIsolationLevel = IsolationLevel.Serializable                
+                DefaultIsolationLevel = IsolationLevel.Serializable
             }.ToString();
 
             connection = new SQLiteConnection(connectionString);
@@ -37,7 +39,7 @@ namespace SharedLib
         }
         public override void Close(bool isWait = false)
         {
-  //          $"[{GetType()} -{ GetHashCode()}] close connection".WriteLogMessage();
+            //          $"[{GetType()} -{ GetHashCode()}] close connection".WriteLogMessage();
             //Зупиняємо всі запити до БД і чекаємо 1/4 секунди. щоб встигли завершитись запити.
 
             if (isWait)
@@ -57,51 +59,81 @@ namespace SharedLib
             SetLock(false);
         }
 
-        void WaitCollect(int pMs=150)
+        void WaitCollect(int pMs = 150)
         {
             try
             {
                 GC.Collect();
                 GC.WaitForPendingFinalizers();
                 Thread.Sleep(pMs);
-            }catch (Exception e ) 
+            } catch (Exception e)
             {
                 var s = e.Message;
             }
         }
 
-        public override IEnumerable<T1> Execute<T,T1>(string query, T parameters )
-        {
-            if (IsLock) ExceptionIsLock();
-            return connection.Query<T1>(query, parameters);
-        }
-
-        public override IEnumerable<T1> Execute<T1>(string query)
-        {
-            if (IsLock) ExceptionIsLock();
-            return connection.Query<T1>(query);
-        }
-
-        public override  Task<IEnumerable<T1>> ExecuteAsync<T, T1>(string query, T parameters)
-        {
-            if (IsLock) ExceptionIsLock();
-            return  connection.QueryAsync<T1>(query, parameters);
-        }
-
-        public override Task<IEnumerable<T1>> ExecuteAsync<T1>(string query)
-        {
-            if (IsLock) ExceptionIsLock();
-            return connection.QueryAsync<T1>(query);
-        }
         public override void BeginTransaction()
         {
-             transaction= connection.BeginTransaction();
+            transaction = connection.BeginTransaction();
         }
 
         public override void CommitTransaction()
         {
             transaction.Commit();
         }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    if (connection != null)
+                    {
+                        connection.Close();
+                        connection.Dispose();
+                        connection = null;
+                    }
+                }
+                disposedValue = true;
+            }
+            //           $"[{GetType()} -{ GetHashCode()}] Dispose connection".WriteLogMessage();
+        }
+
+        void IDisposable.Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+
+        public override IEnumerable<T1> Execute<T, T1>(string query, T parameters)
+        {
+            try {
+                if (IsLock) ExceptionIsLock();
+                return connection.Query<T1>(query, parameters);
+            } catch (Exception e)
+            {
+                FileLogger.WriteLogMessage(this, System.Reflection.MethodBase.GetCurrentMethod().Name + Environment.NewLine + query, e);
+                throw;
+            }
+        }
+    
+
+        public override IEnumerable<T1> Execute<T1>(string query)
+        {
+            try { 
+            if (IsLock) ExceptionIsLock();
+            return connection.Query<T1>(query);
+            }
+            catch (Exception e)
+            {
+                FileLogger.WriteLogMessage(this, System.Reflection.MethodBase.GetCurrentMethod().Name + Environment.NewLine + query, e);
+                throw;
+            }
+        }
+
 
         public override int ExecuteNonQuery<T>(string pQuery, T Parameters, int CountTry = 3)
         {
@@ -153,53 +185,47 @@ namespace SharedLib
             }
         }
 
-        public override Task<int> ExecuteNonQueryAsync<T>(string parQuery, T Parameters)
-        {
-            if (IsLock) ExceptionIsLock();
-            if (TypeCommit == eTypeCommit.Auto)
-                return connection.ExecuteAsync(parQuery, Parameters);
-            else
-                return connection.ExecuteAsync(parQuery, Parameters, transaction);
-        }
-        public override Task<int> ExecuteNonQueryAsync(string parQuery)
-        {
-            if (IsLock) ExceptionIsLock();
-            if (TypeCommit == eTypeCommit.Auto)
-                return connection.ExecuteAsync(parQuery);
-            else
-                return connection.ExecuteAsync(parQuery, null, transaction);
-        }
-
         public override T1 ExecuteScalar<T1>(string query)
         {
-            if (IsLock) ExceptionIsLock();
-            return connection.ExecuteScalar<T1>(query);
+            try
+            {
+                if (IsLock) ExceptionIsLock();
+                return connection.ExecuteScalar<T1>(query);
+            }
+            catch (Exception e)
+            {
+                FileLogger.WriteLogMessage(this, System.Reflection.MethodBase.GetCurrentMethod().Name + Environment.NewLine + query, e);
+                throw;
+            }
         }
 
         public override T1 ExecuteScalar<T,T1>(string query,T parameters)
         {
-            if (IsLock) ExceptionIsLock();
-            return connection.ExecuteScalar<T1>(query, parameters);
-        }
+            try
+            {
+                if (IsLock) ExceptionIsLock();
+                return connection.ExecuteScalar<T1>(query, parameters);
+            }
+            catch (Exception e)
+            {
+                FileLogger.WriteLogMessage(this, System.Reflection.MethodBase.GetCurrentMethod().Name + Environment.NewLine + query, e);
+                throw;
+            }
+    }
 
-        public override Task<T1> ExecuteScalarAsync<T1>(string query)
+        public  int ExecuteNonQuery<T>(string pQuery, T Parameters, SQLiteTransaction transaction)
         {
-            if (IsLock) ExceptionIsLock();
-            return connection.ExecuteScalarAsync<T1>(query);
-        }
-
-        public override Task<T1> ExecuteScalarAsync<T, T1>(string query, T parameters)
-        {
-            if (IsLock) ExceptionIsLock();
-            return connection.ExecuteScalarAsync<T1>(query, parameters);
-        }
-
-
-        public  int ExecuteNonQuery<T>(string parQuery, T Parameters, SQLiteTransaction transaction)
-        {
-            if (IsLock) ExceptionIsLock();
-            return connection.Execute(parQuery, Parameters, transaction);
-        }
+            try
+            {
+                if (IsLock) ExceptionIsLock();
+                return connection.Execute(pQuery, Parameters, transaction);
+            }
+            catch (Exception e)
+            {
+                FileLogger.WriteLogMessage(this, System.Reflection.MethodBase.GetCurrentMethod().Name + Environment.NewLine + pQuery, e);
+                throw;
+            }
+    }
 
         public override int BulkExecuteNonQuery<T>(string pQuery, IEnumerable<T> pData, bool IsRepeatNotBulk = false)
         {
@@ -231,36 +257,50 @@ namespace SharedLib
             return pData.Count();
         }
 
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!disposedValue)
-            {
-                if (disposing)
-                {
-                    if (connection != null)
-                    {
-                        connection.Close();
-                        connection.Dispose();  
-                        connection = null;
-                    }
-                } 
-                disposedValue = true;
-            }
- //           $"[{GetType()} -{ GetHashCode()}] Dispose connection".WriteLogMessage();
-        }
+       
+        /*
 
-        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
-        // ~SQLite()
-        // {
-        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-        //     Dispose(disposing: false);
-        // }
+          public override  Task<IEnumerable<T1>> ExecuteAsync<T, T1>(string query, T parameters)
+   {
+       if (IsLock) ExceptionIsLock();
+       return  connection.QueryAsync<T1>(query, parameters);
+   }
 
-        void IDisposable.Dispose()
-        {
-            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-            Dispose(disposing: true);
-            GC.SuppressFinalize(this);
-        }
+   public override Task<IEnumerable<T1>> ExecuteAsync<T1>(string query)
+   {
+       if (IsLock) ExceptionIsLock();
+       return connection.QueryAsync<T1>(query);
+   }
+
+     public override Task<int> ExecuteNonQueryAsync<T>(string parQuery, T Parameters)
+    {
+        if (IsLock) ExceptionIsLock();
+        if (TypeCommit == eTypeCommit.Auto)
+            return connection.ExecuteAsync(parQuery, Parameters);
+        else
+            return connection.ExecuteAsync(parQuery, Parameters, transaction);
+    }
+    public override Task<int> ExecuteNonQueryAsync(string parQuery)
+    {
+        if (IsLock) ExceptionIsLock();
+        if (TypeCommit == eTypeCommit.Auto)
+            return connection.ExecuteAsync(parQuery);
+        else
+            return connection.ExecuteAsync(parQuery, null, transaction);
+    }
+
+    public override Task<T1> ExecuteScalarAsync<T1>(string query)
+    {
+        if (IsLock) ExceptionIsLock();
+        return connection.ExecuteScalarAsync<T1>(query);
+    }
+
+   public override Task<T1> ExecuteScalarAsync<T, T1>(string query, T parameters)
+   {
+       if (IsLock) ExceptionIsLock();
+       return connection.ExecuteScalarAsync<T1>(query, parameters);
+   }
+    */
+
     }
 }
