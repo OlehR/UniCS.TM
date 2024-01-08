2eac7717f63eb18945c013dd026fb3a92931a91d
Oleg Rutkovskyi <OlehRut@gmail.com>
2024-01-08 08:01:05 +0200

    fix

--- Front/MainWindow_BL.cs
+++ Front/MainWindow_BL.cs
@@ -722,7 +722,7 @@ namespace Front
                         var res = Bl.ds.CheckExciseStamp(new ExciseStamp(CurWares, pES));                    
                         if (res != null)
                         {
-                            if (!res.Equals(CurWares))
+                            if (!res.Equals(CurWares) && res.State>=0)
                             {
                                 CustomMessage.Show($"Дана акцизна марка вже використана {res.CodePeriod} {res.IdWorkplace} Чек=>{res.CodeReceipt} CodeWares=>{res.CodeWares}!", "Увага", eTypeMessage.Error);
                                 return;
