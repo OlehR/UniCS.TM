2a6f19201e8c0304b2ae682cd730a9fd16a840bb
Oleg Rutkovskyi <OlehRut@gmail.com>
2024-01-08 08:47:16 +0200

    fix

--- SharedLib/DataSync.cs
+++ SharedLib/DataSync.cs
@@ -105,11 +105,11 @@ namespace SharedLib
                 FileLogger.WriteLogMessage(this, "SendReceiptTo1C", $"Обмін заблоковано CodeReceipt=>{pIdR.CodeReceipt}", eTypeLog.Expanded);
                 return false;
             }
-            var r = ldb.ViewReceipt(pIdR, true);
+            var R = ldb.ViewReceipt(pIdR, true);
 
 #pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-            _= SendReceiptTo1CAsync(r);
-            _ = SendReceipt(pR); // В сховище чеків
+            _= SendReceiptTo1CAsync(R);
+            _ = SendReceipt(R); // В сховище чеків
 
 #pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
             return true;
