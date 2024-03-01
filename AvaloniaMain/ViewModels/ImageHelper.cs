a63a22768eb2713a4190a010af14b3c78814b371
Денис Станіслав <125273149+denstan2004@users.noreply.github.com>
2024-03-01 14:21:06 +0200

    Added Categories Name

---
+++ AvaloniaMain/ViewModels/ImageHelper.cs
@@ -0,0 +1,52 @@
+﻿using Avalonia.Platform;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Avalonia;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+using System.Threading.Tasks;
+
+namespace AvaloniaMain.ViewModels
+{
+    public static class ImageHelper
+    {
+        public static Bitmap LoadFromFile(string filePath)
+        {
+            try
+            {
+                return new Bitmap(filePath);
+            }
+            catch (Exception ex)
+            {
+                // Обробка помилок завантаження зображення
+                Console.WriteLine($"Error loading image from file '{filePath}': {ex.Message}");
+                return null;
+            }
+        }
+
+        public static async Task<Bitmap?> LoadFromWeb(Uri url)
+        {
+            using var httpClient = new HttpClient();
+            try
+            {
+                var response = await httpClient.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                var data = await response.Content.ReadAsByteArrayAsync();
+                return new Bitmap(new MemoryStream(data));
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine($"An error occurred while downloading image '{url}' : {ex.Message}");
+                return null;
+            }
+        }
+    }
+}
--- AvaloniaMain/ViewModels/SearchViewModel.cs
+++ AvaloniaMain/ViewModels/SearchViewModel.cs
@@ -1,11 +1,15 @@
 ﻿using Avalonia.Controls.Primitives;
+using Avalonia.Media.Imaging;
+using ModelMID;
 using ReactiveUI;
 using SharedLib;
 using System;
 using System.Collections.Generic;
+
 using System.Linq;
 using System.Reactive;
 using System.Text;
+
 using System.Threading.Tasks;
 
 namespace AvaloniaMain.ViewModels
@@ -13,11 +17,92 @@ namespace AvaloniaMain.ViewModels
     internal class SearchViewModel : ViewModelBase
     {
         BL Bl;
+        private string _path = "C:/Pictures/Categories\\000009001.jpg";
+        public Bitmap _imageFromBinding;
+        public Bitmap ImageFromBinding
+        {
+            get => _imageFromBinding;
+            set
+            {
+                if (_imageFromBinding != value)
+                {
+                    _imageFromBinding = value;
+                    OnPropertyChanged(nameof(ImageFromBinding));
+                }
+            }
+        }
+        public string path 
+        {
+            get => _path;
+            set
+            {
+                if (_path != value)
+                {
+                    _path = value;
+                    OnPropertyChanged(nameof(path));
+                }
+            }
+        }
         int CodeFastGroup = 0;
         int OffSet = 0;
         int MaxPage = 0;
-        int Limit = 12;
+        int Limit = 10;
+        int Current = 0;
+        private List<GW> _onPageProductsTop;
+        public List<GW> OnPageProductsTop
+        {
+            get => _onPageProductsTop;
+            set
+            {
+                if (_onPageProductsTop != value)
+                {
+                    _onPageProductsTop = value;
+                    OnPropertyChanged(nameof(OnPageProductsTop));
+                }
+            }
+        }
 
+        private List<GW> _onPageProductsBottom;
+        public List<GW> OnPageProductsBottom
+        {
+            get => _onPageProductsBottom;
+            set
+            {
+                if (_onPageProductsBottom != value)
+                {
+                    _onPageProductsBottom = value;
+                    OnPropertyChanged(nameof(OnPageProductsBottom));
+                }
+            }
+        }
+
+        private List<GW> _onPageProducts;
+        public List<GW> OnPageProducts
+        {
+            get => _onPageProducts;
+            set
+            {
+                if (_onPageProducts != value)
+                {
+                    _onPageProducts = value;
+                    OnPropertyChanged(nameof(OnPageProducts));
+                }
+            }
+        }
+
+        private List<GW> _AllProducts;
+        public List<GW> AllProducts
+        {
+            get => _AllProducts;
+            set
+            {
+                if (_AllProducts != value)
+                {
+                    _AllProducts = value;
+                    OnPropertyChanged(nameof(AllProducts));
+                }
+            }
+        }
         public event EventHandler? VisibilityChanged;
         private ViewModelBase _currentPage;
         public ViewModelBase CurrentPage
@@ -49,11 +134,23 @@ namespace AvaloniaMain.ViewModels
 
         public SearchViewModel()
         {
+            try
+            {
+                ImageFromBinding =
+                    ImageHelper.LoadFromFile("C:/Pictures/Categories/000009001.jpg");
+            }
+            catch (Exception ex) { }
+            
             var viewModel = new KeyBoardViewModel();
             viewModel.TextChanged += KeyBoard_TextChanged;
             CurrentPage = viewModel;
             Bl = BL.GetBL;
             var Res=Bl.GetDataFindWares(CodeFastGroup, CurrentText,new ModelMID.IdReceipt(),ref OffSet,ref MaxPage,ref Limit);
+            AllProducts = (List<GW>)Res;
+            OnPageProducts = AllProducts.GetRange(Limit * Current, Limit);
+            UpdatePageProducts();
+
+
         }
 
         private void KeyBoard_TextChanged(object? sender, string text)
@@ -66,6 +163,14 @@ namespace AvaloniaMain.ViewModels
         private async Task Close()
         {
             VisibilityChanged.Invoke(this, EventArgs.Empty);
+        }
+        private void UpdatePageProducts()
+        {
+            if (OnPageProducts != null )
+            {
+                OnPageProductsTop = OnPageProducts.GetRange(0, Limit / 2);
+                OnPageProductsBottom = OnPageProducts.GetRange(Limit / 2, Limit / 2);
+            }
         }
     }
 }
