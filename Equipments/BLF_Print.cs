c8fd772b966afd8d008a9763bdcf3b7b4c814f0b
sevcukd <sevcukd9@gmail.com>
2025-01-23 16:36:49 +0200

    Правки по замовленню покупця

--- Equipments/BLF_Print.cs
+++ Equipments/BLF_Print.cs
@@ -20,11 +20,11 @@ namespace Front.Equipments
         {
             //MW.curReceipt.Client.SumMoneyBonus = 100;
             var xx = Bl.db.GetNoPricePromorion(MW.curReceipt).Where(x => x.TypeDiscount == eTypeDiscount.TextСashier).FirstOrDefault();
-            if(MW.curReceipt?.Client!=null && xx!=null)
+            if (MW.curReceipt?.Client != null && xx != null)
             {
                 SetStateView(eStateMainWindows.WaitCustomWindows, eTypeAccess.NoDefine, null, new CustomWindow(eWindows.NoPricePromotion, xx));
                 return;
-            }          
+            }
 
             if (Global.Settings.MaxSumReceipt > 0 && MW.curReceipt.SumTotal > Global.Settings.MaxSumReceipt)
             {
@@ -185,39 +185,9 @@ namespace Front.Equipments
                     }
                     finally { R.IdWorkplacePay = 0; }
                 }
+                //Друк номерка на піцу якщо він потрібний
+                PrintOrderReceipt(R);
 
-                {
-                    bool IsNeadOrderReceipt = R.Wares.Where(x=>x.ProductionLocation != 0).Any();
-                    if (Global.IsPrintOrderReceipt && IsNeadOrderReceipt)
-                    {
-
-
-                        Task.Run(async () =>
-                        {
-                            CommandAPI<Receipt> Command = new() { Command = eCommand.GetOrderNumber, Data = R };
-
-                            try
-                            {
-                                var r = new SocketClient(IPAddress.Parse(Global.IPAddressOrderService), 3444);
-                                var Ansver = await r.StartAsync(Command.ToJson());
-                                List<string> list = new List<string>() { "Номер замовлення:", $"{Ansver.TextState}" };
-                                var res = EF.PrintNoFiscalReceipt(R, list);
-                                List<string> listWares = new List<string>();
-                                listWares = R.Wares.Where(x=> x.ProductionLocation>0).Select(x => $"{x.NameWares} => {x.Quantity}").ToList();
-                                listWares.Insert(0, $"Список замовлення №{Ansver.TextState}");
-                                listWares.Add(DateTime.Now.ToString("g"));
-                                var res2 = EF.PrintNoFiscalReceipt(R, listWares);
-                                FileLogger.WriteLogMessage($"SocketAnsver: {Environment.NewLine}Command: {Command} {Environment.NewLine}IdWorkPlace: {R.IdWorkplace}{Environment.NewLine}Ansver: {Ansver.TextState}", eTypeLog.Full);
-                                //SocketAnsver?.Invoke(eCommand.GetOrderNumber, MainWorkplace, Ansver);
-                            }
-                            catch (Exception ex)
-                            {
-                                FileLogger.WriteLogMessage(this, $"GeneralCondition DNSName=>{IPAddress.Parse(Global.IPAddressOrderService)} {Command} ", ex);
-                                //SocketAnsver?.Invoke(eCommand.GetOrderNumber, MainWorkplace, new Status(ex));
-                            }
-                        });
-                    }
-                }
                 R.StateReceipt = Bl.GetStateReceipt(R);
                 if (R.StateReceipt == eStateReceipt.Pay || R.StateReceipt == eStateReceipt.PartialPrint || R.StateReceipt == eStateReceipt.StartPrint)
                 {
@@ -293,7 +263,45 @@ namespace Front.Equipments
             }
             return Res;
         }
+        private void PrintOrderReceipt(Receipt R, bool IsTryAgain = true)
+        {
+            bool IsNeadOrderReceipt = R.Wares.Where(x => x.ProductionLocation != 0).Any();
+            if (Global.IsPrintOrderReceipt && IsNeadOrderReceipt)
+            {
 
+
+                Task.Run(async () =>
+                {
+                    CommandAPI<Receipt> Command = new() { Command = eCommand.GetOrderNumber, Data = R };
+
+                    try
+                    {
+                        var r = new SocketClient(IPAddress.Parse(Global.IPAddressOrderService), 3444);
+                        var Ansver = await r.StartAsync(Command.ToJson());
+                        if (!Ansver.status && IsTryAgain)
+                        {
+                            FileLogger.WriteLogMessage($"SocketAnsver: {Environment.NewLine}Command: {Command} {Environment.NewLine}IdWorkPlace: {R.IdWorkplace}{Environment.NewLine}Ansver: {Ansver.TextState}{Environment.NewLine} Перша спроба", eTypeLog.Error);
+                            PrintOrderReceipt(R, false);
+                            return;
+                        }
+                        List<string> list = new List<string>() { "Номер замовлення:", $"{Ansver.TextState}" };
+                        var res = EF.PrintNoFiscalReceipt(R, list);
+                        List<string> listWares = new List<string>();
+                        listWares = R.Wares.Where(x => x.ProductionLocation > 0).Select(x => $"{x.NameWares} => {x.Quantity}").ToList();
+                        listWares.Insert(0, $"Список замовлення №{Ansver.TextState}");
+                        listWares.Add(DateTime.Now.ToString("g"));
+                        var res2 = EF.PrintNoFiscalReceipt(R, listWares);
+                        FileLogger.WriteLogMessage($"SocketAnsver: {Environment.NewLine}Command: {Command} {Environment.NewLine}IdWorkPlace: {R.IdWorkplace}{Environment.NewLine}Ansver: {Ansver.TextState}", eTypeLog.Full);
+                        //SocketAnsver?.Invoke(eCommand.GetOrderNumber, MainWorkplace, Ansver);
+                    }
+                    catch (Exception ex)
+                    {
+                        FileLogger.WriteLogMessage(this, $"GeneralCondition DNSName=>{IPAddress.Parse(Global.IPAddressOrderService)} {Command} ", ex);
+                        //SocketAnsver?.Invoke(eCommand.GetOrderNumber, MainWorkplace, new Status(ex));
+                    }
+                });
+            }
+        }
         public void FillPays(Receipt pR)
         {
             int[] IdWorkplacePays = pR.IdWorkplacePays;//Wares.Select(el => el.IdWorkplacePay).Distinct().OrderBy(el => el).ToArray();
