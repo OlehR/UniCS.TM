5f5dee762259d7ad1fce4ac8a9034f8cef27ca51
sevcukd <sevcukd9@gmail.com>
2025-12-12 12:14:45 +0200

    правка повернення

--- SharedLib/BL.cs
+++ SharedLib/BL.cs
@@ -695,7 +695,7 @@ namespace SharedLib
                 var NewR = GetNewIdReceipt(IdR.IdWorkplace);
                 var R = GetReceiptHead(IdR, true);
                 R.AdditionC1 = R.Payment?.Where(x=>x.IsCashBack != true)?.First()?.CodeAuthorization;
-                R.AdditionCashBack = R.Payment?.Where(x => x.IsCashBack == true)?.First()?.CodeAuthorization;
+                R.AdditionCashBack = R.Payment?.Where(x => x.IsCashBack == true)?.FirstOrDefault()?.CodeAuthorization;
                 R.Payment = null;
                 R.ReceiptEvent = null;
                 R.DateReceipt = DateTime.Now;
