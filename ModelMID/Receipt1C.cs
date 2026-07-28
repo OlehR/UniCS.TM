874210ff0cc0f883c2b62c587e36151feafbda85
Олег Рутковський <OlehR@outlook.com>
2026-07-28 15:16:25 +0300

    Кешмашина

--- ModelMID/Receipt1C.cs
+++ ModelMID/Receipt1C.cs
@@ -89,7 +89,7 @@ namespace ModelMID
             SumWallet = pR.Payment.Where(r => r.TypePay == eTypePay.Wallet && r.SumPay > 0)?.FirstOrDefault()?.SumPay ?? 0;
             CashOutSum = pR.Payment.Where(r => r.TypePay == eTypePay.IssueOfCash && r.SumPay > 0)?.FirstOrDefault()?.SumPay ?? 0;
 
-            var Cash = pR.Payment.Where(r => r.TypePay == eTypePay.Cash)?.FirstOrDefault();
+            var Cash = pR.Payment.Where(r => r.TypePay == eTypePay.Cash || r.TypePay == eTypePay.CashMachine)?.FirstOrDefault();
             var PostPaid = pR.Payment.Where(r => r.TypePay == eTypePay.Postpaid)?.FirstOrDefault();
             if (Cash != null) CodeBank = 1;
             else if (PostPaid != null) CodeBank = 14;
